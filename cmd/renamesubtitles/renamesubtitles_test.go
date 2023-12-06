bdc0833a57740c2dbf8ba47a53443e4a146471a6
Elias Menon <eliasmenon@gmail.com>
2023-12-06 02:30:12 -0300

    Add renamesubtitles

---
+++ cmd/renamesubtitles/renamesubtitles_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractChapter(t *testing.T) {
+	assert.EqualValues(t, 41, extractChapterInfoFromFilename("+_-_41_"))
+	assert.EqualValues(t, 2, extractChapterInfoFromFilename(" - 02."))
+	assert.EqualValues(t, 1, extractChapterInfoFromFilename(" - S01E01."))
+	assert.EqualValues(t, 3, extractChapterInfoFromFilename("_-_03."))
+	assert.EqualValues(t, 1, extractChapterInfoFromFilename(" S2 - 01."))
+	assert.EqualValues(t, 4, extractChapterInfoFromFilename(" - S02E04."))
+	assert.EqualValues(t, 4, extractChapterInfoFromFilename("86 - 04."))
+}
